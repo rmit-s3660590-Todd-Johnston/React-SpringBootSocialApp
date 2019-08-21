66e266308d1fd08c08502930765d289a0aa5197d
Jack <jackryan1998@hotmail.com>
2019-08-21 23:21:41 +1000

    icon + name

--- frontend/social-app/src/screens/ProfileScreen.js
+++ frontend/social-app/src/screens/ProfileScreen.js
@@ -1,10 +1,20 @@
 import React, {Component} from 'react';
+import {Avatar } from "antd";
 
 
 export default class ProfileScreen extends Component {
     render() {
         return <div>
-            <h1>Profile</h1>
+        <h1>Profile</h1>
+        <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '10vh'}}>
+    <Avatar src="https://short-biography.com/wp-content/uploads/mark-zuckerberg/Mark-Zuckerberg-300x300.jpg" size={128} icon="user" />
+            </div>
+            <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '10vh'}}>
+    <h1>Mark</h1>
         </div>
+        <div>
+
+        </div>
+        </div>
+
     }
-}
