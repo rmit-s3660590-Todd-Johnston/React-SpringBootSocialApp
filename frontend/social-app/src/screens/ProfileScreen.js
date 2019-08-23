716f849be48d41f5f0a9d3732ceee80960741f82
Jack <jackryan1998@hotmail.com>
2019-08-23 23:57:12 +1000

    Update ProfileScreen.js

--- frontend/social-app/src/screens/ProfileScreen.js
+++ frontend/social-app/src/screens/ProfileScreen.js
@@ -1,9 +1,34 @@
 import React, {Component} from 'react';
-import {Avatar,Descriptions, Divider, Tooltip,Button} from "antd";
+import {Avatar,Descriptions, Divider, Tooltip,Button, List, Card} from "antd";
 
+const data = [
+    {
+        title: 'Title 1',
+    },
+    {
+        title: 'Title 2',
+    },
+    {
+        title: 'Title 3',
+    },
+    {
+        title: 'Title 4',
+    },
+    {
+        title: 'Title 5',
+    },
+    {
+        title: 'Title 6',
+    },
+];
 
 
 export default class ProfileScreen extends Component {
+    state = {
+
+    }
+
+
     render() {
         return <div>
             <h1>Profile</h1>
@@ -17,12 +42,12 @@ export default class ProfileScreen extends Component {
                 <h1>Mark</h1>
             </div>
             <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-                <Button size={'large'} style={{"background-color" : "blue"}}>
+                <Button style={{marginRight: '60px'}} size={'large'} style={{"background-color" : "blue"}}>
                     Message Me
                 </Button>
-                <Button size={'large'}>Add Friend?</Button>
+                <Button style={{marginLeft: '60px'}} size={'large'} >Add Friend?</Button>
             </div>
-            <Divider>About Me</Divider>
+            <Divider style={{color:'black', font:'bold', marginTop:'30px'}}>About Me</Divider>
             <Descriptions>
                 <Descriptions.Item label="UserName">Mark</Descriptions.Item>
                 <Descriptions.Item label="Telephone">9876543234</Descriptions.Item>
@@ -30,6 +55,24 @@ export default class ProfileScreen extends Component {
                 <Descriptions.Item label="Address"> Planet Earth
                 </Descriptions.Item>
             </Descriptions>
+            <Divider style={{color:'black', font:'bold', marginTop:'30px'}}>My Skills</Divider>
+            <List
+                grid={{
+                    gutter: 16,
+                    xs: 1,
+                    sm: 2,
+                    md: 4,
+                    lg: 4,
+                    xl: 6,
+                    xxl: 3,
+                }}
+                dataSource={data}
+                renderItem={item => (
+                    <List.Item>
+                        <Card title={item.title}>Card content</Card>
+                    </List.Item>
+                )}
+            />
             <div>
             </div>
         </div>
