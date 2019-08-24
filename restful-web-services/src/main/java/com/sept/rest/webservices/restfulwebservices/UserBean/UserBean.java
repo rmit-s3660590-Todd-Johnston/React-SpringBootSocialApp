7429ce6dcc369011ec91ddd7fafafe72affb2bca
Shahrzad Rafezi <shahrzad_rafezi@yahoo.com>
2019-08-25 09:58:25 +1000

    Added some methods in UserBean

--- restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/UserBean/UserBean.java
+++ restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/UserBean/UserBean.java
@@ -1,4 +1,10 @@
 package com.sept.rest.webservices.restfulwebservices.UserBean;
 
 public class UserBean {
+
+    private String message;
+
+    public UserBean(String msg) {
+        this.message = msg;
+    }
 }
--- restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/UserBean/UserBeanController.java
+++ restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/UserBean/UserBeanController.java
@@ -1,4 +1,17 @@
 package com.sept.rest.webservices.restfulwebservices.UserBean;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller
+@RestController
+@CrossOrigin(origins="http://localhost:4200")
+
 public class UserBeanController {
+    @GetMapping(path = "/user")
+
+    public UserBean helloWorld() {
+        return new UserBean("Test");
+    }
 }
