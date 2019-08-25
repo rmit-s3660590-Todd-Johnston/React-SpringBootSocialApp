780046289387e26ededc8031f79f48e56b41d2cb
Shahrzad Rafezi <shahrzad_rafezi@yahoo.com>
2019-08-25 10:48:44 +1000

    Changed the UserBean class
    Added id, username, name and last name

--- restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/UserBean/UserBean.java
+++ restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/UserBean/UserBean.java
@@ -2,9 +2,45 @@ package com.sept.rest.webservices.restfulwebservices.UserBean;
 
 public class UserBean {
 
-    private String message;
+    private int id;
+    private String username;
+    private String name;
+    private String lastName;
 
-    public UserBean(String msg) {
-        this.message = msg;
+
+    public UserBean() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
