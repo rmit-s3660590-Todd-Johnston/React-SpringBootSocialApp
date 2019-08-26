f6032c78a8c8d412f5fdd90c92491e4ed32b155e
Jack <jackryan1998@hotmail.com>
2019-08-26 10:22:59 +1000

    Profile Page + test

--- frontend/social-app/src/screens/ProfileScreen.js
+++ frontend/social-app/src/screens/ProfileScreen.js
@@ -2,33 +2,17 @@ import React, {Component} from 'react';
 import axios from 'axios'
 import {Avatar, Descriptions, Divider, Tooltip, Button, List, Card, Badge, Icon} from "antd";
 
-const data = [
-    {
-        title: 'Skill 1',
-    },
-    {
-        title: 'Skill 2',
-    },
-    {
-        title: 'Skill 3',
-    },
-    {
-        title: 'Skill 4',
-    },
-    {
-        title: 'Skill 5',
-    },
-    {
-        title: 'Skill 6',
-    },
-];
-
+const gridStyle = {
+    width: '25%',
+    textAlign: 'center',
+    backgroundColor: 'lightgray',
+};
 
 export default class ProfileScreen extends Component {
    constructor(props){
        super(props);
        this.state = {
-           name: 'Name Error',
+           name: 'Mark',
            profilePic: "https://short-biography.com/wp-content/uploads/mark-zuckerberg/Mark-Zuckerberg-300x300.jpg"
        };
    }
@@ -46,28 +30,27 @@ export default class ProfileScreen extends Component {
         return <div>
             <h1>Profile</h1>
             <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '10vh'}}>
-                    <a href={"#"}>
-                    <Badge count={<Icon style={{marginBottom : '10px'}} type={"edit"}/>}>
+                <a href={"#"}>
+                    <Badge count={<Icon style={{marginBottom : '10px', color:'gray'}} type={"edit"}/>}>
                         <Tooltip placement="right" title={"Hey! Don't touch my face human"}>
                         <Avatar src= {this.state.profilePic}
                         size={128} icon="user"/>
                         </Tooltip>
                     </Badge>
                     </a>
-
-
             </div>
 
             <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '10vh'}}>
                 <h1>{this.state.name}</h1>
-
             </div>
+
             <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-                <Button style={{marginRight: '60px'}} size={'large'} style={{"background-color" : "blue"}}>
+                <Button style={{marginRight: '60px', backgroundColor : "aqua"}} size={'large'}>
                     Message Me
                 </Button>
                 <Button style={{marginLeft: '60px'}} size={'large'} >Add Friend?</Button>
             </div>
+
             <Divider style={{color:'black', font:'bold', marginTop:'30px'}}>About Me</Divider>
             <Descriptions>
                 <Descriptions.Item label="UserName">Mark</Descriptions.Item>
@@ -76,26 +59,13 @@ export default class ProfileScreen extends Component {
                 <Descriptions.Item label="Address"> Planet Earth
                 </Descriptions.Item>
             </Descriptions>
+
             <Divider style={{color:'black', font:'bold', marginTop:'30px'}}>My Skills</Divider>
-            <List
-                grid={{
-                    gutter: 16,
-                    xs: 1,
-                    sm: 2,
-                    md: 4,
-                    lg: 4,
-                    xl: 6,
-                    xxl: 3,
-                }}
-                dataSource={data}
-                renderItem={item => (
-                    <List.Item>
-                        <Card title={item.title}>Card content</Card>
-                    </List.Item>
-                )}
-            />
-            <div>
-            </div>
+            <Card.Grid style={gridStyle}>Business Man</Card.Grid>
+            <Card.Grid style={gridStyle}>Decision Maker</Card.Grid>
+            <Card.Grid style={gridStyle}>Deal Closer</Card.Grid>
+            <Card.Grid style={gridStyle}>Offer Maker</Card.Grid>
+            <Card.Grid style={gridStyle}>Machine Learning</Card.Grid>
         </div>
     }
 }
