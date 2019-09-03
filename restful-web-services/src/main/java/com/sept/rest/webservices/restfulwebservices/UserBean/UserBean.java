95765299b6adaeeb04ad58fa1262d3c9608eb680
Shahrzad Rafezi <shahrzad_rafezi@yahoo.com>
2019-09-03 17:04:56 +1000

    Improved more on the model and controller

--- restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/UserBean/UserBean.java
+++ restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/UserBean/UserBean.java
@@ -1,36 +1,48 @@
 package com.sept.rest.webservices.restfulwebservices.UserBean;
-
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Entity
+@Table(name = "userBean")
 public class UserBean {
 
     @Id
-    private String id;
-    private String username;
+    @GeneratedValue
+    private Long id;
+    @NotBlank
+    private String user_name;
+    @NotBlank
     private String name;
-    private String lastName;
+    @NotBlank
+    private String last_name;
 
 
     public UserBean() {
-
+        super();
     }
 
-    public String getId() {
+    public UserBean(Long id, String user_name, String name, String last_name){
+        super();
+        this.id = id;
+        this.user_name = user_name;
+        this.name = name;
+        this.last_name = last_name;
+    }
+
+    public Long getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
-    public String getUsername() {
-        return username;
+    public String getUser_name() {
+        return user_name;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
     }
 
     public String getName() {
@@ -41,11 +53,11 @@ public class UserBean {
         this.name = name;
     }
 
-    public String getLastName() {
-        return lastName;
+    public String getLast_name() {
+        return last_name;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
     }
 }
---
+++ restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/UserBean/UserBeanRepository.java
@@ -0,0 +1,11 @@
+package com.sept.rest.webservices.restfulwebservices.UserBean;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+// this interface deals with database management in Java.
+//Annotation used to Indicate the DAO (Data Access Object) component in the persistence layer.
+@Repository
+public interface UserBeanRepository extends JpaRepository<UserBean, Long> {
+
+}
