021e41f05c4cbb60e8435d26c8be6b0000dc66d5
Shahrzad Rafezi <shahrzad_rafezi@yahoo.com>
2019-09-03 17:26:25 +1000

    Added a query in userBean

--- restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/UserBean/UserBeanRepository.java
+++ restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/UserBean/UserBeanRepository.java
@@ -2,10 +2,13 @@ package com.sept.rest.webservices.restfulwebservices.UserBean;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 // this interface deals with database management in Java.
+//this is where you do queries
 //Annotation used to Indicate the DAO (Data Access Object) component in the persistence layer.
 @Repository
 public interface UserBeanRepository extends JpaRepository<UserBean, Long> {
+    List<UserBean> findAllBy(Long id);
 
 }
