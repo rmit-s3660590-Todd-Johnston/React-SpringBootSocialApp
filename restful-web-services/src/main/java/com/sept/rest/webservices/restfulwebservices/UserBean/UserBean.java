06fa3321602a4995629ccfee70cd8613d101d124
Todd Johnston <s3660590@student.rmit.edu.au>
2019-09-04 15:46:11 +1000

    Added mentor and subject attribute to user
    also included methods for adding and removing classes/subjects from a user

--- restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/UserBean/UserBean.java
+++ restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/UserBean/UserBean.java
@@ -17,19 +17,27 @@ public class UserBean {
     private String last_name;
     @NotBlank
     private String password;
+    @NotBlank
+    private boolean isMentor;
+    @NotBlank
+    private String[] subjects;
 
 
     public UserBean() {
         super();
     }
 
-    public UserBean(Long id, String user_name, String name, String last_name, String password){
+    public UserBean(Long id, String user_name, String name, String last_name, String password, boolean isMentor){
         super();
         this.id = id;
         this.user_name = user_name;
         this.name = name;
         this.last_name = last_name;
         this.password = password;
+        this.isMentor = isMentor;
+        this.subjects = new String[4];//uninitialised list of classes, to be done via addclass method after user creation
+        //max classes/subjects
+
     }
 
     public Long getId() {
@@ -70,5 +78,43 @@ public class UserBean {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public void setMentor(boolean isMentor)
+    {
+        this.isMentor = isMentor;
+    }
+
+    public boolean getMentor()
+    {
+        return this.isMentor;
+    }
+
+    public String addSubject(String subject)
+    {
+        //checking if all class slots are taken
+        for(int i = 0; i< this.subjects.length; i++)
+        {
+            if(this.subjects[i]==null)
+            {
+               this.subjects[i] = subject;
+                return "Subject Added Successfully";
+            }
+        }
+        //if no slot is found
+        return "Subjects full! Please remove a class before adding another";
+    }
+
+    public String removeSubject(String subject)
+    {
+        for(int i = 0; i< this.subjects.length; i++)
+        {
+            if(this.subjects[i].equals(subject))
+            {
+                this.subjects[i] = null;
+                return "Subject removed Successfully";
+            }
+        }
+        return "Unable to find subject";
     }
 }
