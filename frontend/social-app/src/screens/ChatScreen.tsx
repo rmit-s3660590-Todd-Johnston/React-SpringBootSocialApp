9c64ae0f7c3ff2a767671e6c33406b117a650100
Black-Tomcat <tomcat.frantz@gmail.com>
2019-09-10 10:32:13 +1000

    Basic chat and chat window added

--- frontend/social-app/src/screens/ChatScreen.tsx
+++ frontend/social-app/src/screens/ChatScreen.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import {Input, Layout, Menu, Typography} from "antd";
 import Message from "../components/Message";
+import ChatWindow from "../containers/ChatWindow";
 
 
 interface ChatScreenProps {
@@ -8,7 +9,6 @@ interface ChatScreenProps {
 
 const ChatScreen: React.FC<ChatScreenProps> = (props: ChatScreenProps) => {
     const [openChat, setOpenChat] = useState('0');
-    const [inputValue, setInputValue] = useState("");
 
     return <Layout>
         <Layout.Sider>
@@ -27,35 +27,7 @@ const ChatScreen: React.FC<ChatScreenProps> = (props: ChatScreenProps) => {
                 </Menu.Item>)}
             </Menu>
         </Layout.Sider>
-        <Layout.Content style={{
-            backgroundColor: "#FFFFFF",
-            margin: 26,
-            padding: 26,
-            display: 'flex',
-            flexDirection: "column"
-        }}>
-            <div style={{overflowY: "scroll", maxHeight: '-webkit-fill-available', flexGrow: 1}}>
-                <Message
-                    owned={false}
-                    message={"Hello from the other side!"}
-                />
-                <Message
-                    owned={true}
-                    message={"Hello from me!"}
-                />
-                <Message
-                    owned={true}
-                    message={"Hello from me!"}
-                />
-            </div>
-            <Input.Search
-                value={inputValue}
-                onChange={(e) => {setInputValue(e.target.value)}}
-                onSearch={() => {console.info(inputValue); setInputValue("")}}
-                onPressEnter={() => {console.info(inputValue); setInputValue("")}}
-                enterButton={"Send"}
-            />
-        </Layout.Content>
+        <ChatWindow/>
     </Layout>;
 };
 
