f7f1ddcd7494ba8b168f8c52f18572486eac6e89
Jack <jackryan1998@hotmail.com>
2019-09-13 14:23:28 +1000

    Update Search.js

--- frontend/social-app/src/components/Search.js
+++ frontend/social-app/src/components/Search.js
@@ -6,12 +6,71 @@ export default class Search extends Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            value: '',
+            isFocus: false,
+            isClearing: false,
+        };
+
+        this.handleFocus = this.handleFocus.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleMouseDown = this.handleMouseDown.bind(this);
+        this.handleMouseLeave = this.handleMouseLeave.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+        this.handlePressEnter = this.handlePressEnter.bind(this);
+    }
+
+    handleFocus(e) {
+        if (!this.state.isClearing) {
+            this.setState({ isFocus: true });
+            const onFocus = this.props.onFocus;
+            if (onFocus) {
+                onFocus(e);
+            }
+        }
+    }
+
+    handleInputChange(e) {
+        const value = e.target.value;
+        this.setState({ value }, () => {
+            const onChange = this.props.onChange;
+            if (onChange) {
+                onChange(value);
+            }
+        });
+    }
+
+    handleMouseDown() {
+        this.setState({ isClearing: true });
+    }
+
+    handleMouseLeave() {
+        this.Input.refs.input.focus();
+        this.setState({ isClearing: false });
+    }
+
+    handleClick() {
+        this.setState({ value: '' }, () => {
+            const onChange = this.props.onChange;
+            if (onChange) {
+                onChange('');
+            }
+            this.Input.refs.input.focus();
+            this.setState({ isClearing: false });
+        });
+    }
+
+    handlePressEnter() {
+        const onSearch = this.props.onSearch;
+        if (onSearch) {
+            onSearch(this.state.value);
+        }
     }
 
     componentDidMount() {
         this.setState({isLoading: true});
 
-        fetch('api/groups')
+        fetch('api/')
             .then(response => response.json())
             .then(data => this.setState({groups: data, isLoading: false}));
     }
