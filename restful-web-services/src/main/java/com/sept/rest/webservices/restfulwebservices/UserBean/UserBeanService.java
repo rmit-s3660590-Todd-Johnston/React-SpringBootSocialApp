c61bf60e8ea25b30883a5ce646e5fe789fe84b69
Todd Johnston <s3660590@student.rmit.edu.au>
2019-09-22 16:24:09 +1000

    Modified findall method to interact with repository in service

--- restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/UserBean/UserBeanService.java
+++ restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/UserBean/UserBeanService.java
@@ -1,17 +1,24 @@
 package com.sept.rest.webservices.restfulwebservices.UserBean;
 
-import java.util.ArrayList;
-import java.util.List;
-import org.springframework.stereotype.Service;
+		import java.util.ArrayList;
+		import java.util.List;
+
+		import org.springframework.beans.factory.annotation.Autowired;
+		import org.springframework.stereotype.Service;
 
 @Service
 public class UserBeanService {
 
+	@Autowired
+	UserBeanRepository userBeanRepository;
 	private static List<UserBean> UserBeans = new ArrayList<>();
 	private static long idCounter = 0;
 
 	public List<UserBean> findAll() {
-		return UserBeans;
+		return userBeanRepository.findAll();
 	}
+
+
+
 
 }
