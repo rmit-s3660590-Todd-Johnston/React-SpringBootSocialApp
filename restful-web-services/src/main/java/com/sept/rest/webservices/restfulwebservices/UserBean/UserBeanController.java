6b80b01647944fa00ce602a8013dd008976ee9a2
Jack <jackryan1998@hotmail.com>
2019-09-23 15:48:30 +1000

    gitig

--- restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/UserBean/UserBeanController.java
+++ restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/UserBean/UserBeanController.java
@@ -1,6 +1,5 @@
 package com.sept.rest.webservices.restfulwebservices.UserBean;
 
-import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
