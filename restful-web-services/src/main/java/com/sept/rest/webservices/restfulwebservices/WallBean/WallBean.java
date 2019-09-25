4fcf294416793267fad6fb7df12fdd85023dd556
Shahrzad Rafezi <shahrzad_rafezi@yahoo.com>
2019-09-25 16:00:13 +1000

    Started WallBean

--- restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/WallBean/WallBean.java
+++ restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/WallBean/WallBean.java
@@ -1,4 +1,26 @@
 package com.sept.rest.webservices.restfulwebservices.WallBean;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "wallBean")
 public class WallBean {
+    @Id
+    @GeneratedValue
+    private String id;
+    private String post;
+
+    public WallBean(String id, String post){
+        this.id = "w" + id;
+        this.post = post;
+    }
+
+    public String getPost() {
+        return post;
+    }
+
+    public void setPost(String post) {
+        this.post = post;
+    }
+
 }
