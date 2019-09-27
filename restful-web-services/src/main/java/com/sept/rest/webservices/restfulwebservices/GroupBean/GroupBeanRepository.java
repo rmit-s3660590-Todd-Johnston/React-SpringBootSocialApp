08dc7a9a351364b69e2d9a46d1a31201bf3809c7
Jack <jackryan1998@hotmail.com>
2019-09-27 10:25:19 +1000

    Revert "Revert "Revert "Merge branch 'BackendAPI's' into UserProfile"""
    This reverts commit 4de53080f25ebf21be376072c4c1fe3a8c670d4c.

--- restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/GroupBean/GroupBeanRepository.java
+++
@@ -1,6 +0,0 @@
-package com.sept.rest.webservices.restfulwebservices.GroupBean;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GroupBeanRepository extends JpaRepository<GroupBean, Long> {
-}
--- restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/WallBean/WallBeanRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sept.rest.webservices.restfulwebservices.WallBean;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WallBeanRepository extends JpaRepository<WallBean, Long> {
-}
