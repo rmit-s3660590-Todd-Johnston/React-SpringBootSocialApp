f7b284d42016bc08e5546eacedaaf9ec11460981
Shahrzad Rafezi <shahrzad_rafezi@yahoo.com>
2019-09-28 14:21:15 +1000

    Added test code for UserBean

--- restful-web-services/src/test/java/com/sept/rest/webservices/restfulwebservices/UserBeanControllerTest.java
+++ restful-web-services/src/test/java/com/sept/rest/webservices/restfulwebservices/UserBeanControllerTest.java
@@ -1,4 +1,34 @@
 package com.sept.rest.webservices.restfulwebservices;
 
+import com.sept.rest.webservices.restfulwebservices.UserBean.UserBean;
+import com.sept.rest.webservices.restfulwebservices.UserBean.UserBeanRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Optional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class UserBeanControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserBeanRepository mockRepository;
+
+    UserBean mockUser = new UserBean(1L, "Sherry1377", "Shahrzad", "Rafezi", "Password1234!", false);
+
+    @Test
+    public void getAllUsers() throws Exception {
+        Mockito.when(mockRepository.findById(1L)).thenReturn(Optional.of(mockUser));
+    }
 }
