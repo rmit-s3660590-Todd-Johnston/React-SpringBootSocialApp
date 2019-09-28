7505c6b28a2e95417142b6bc6a3b51a81f53b20b
Shahrzad Rafezi <shahrzad_rafezi@yahoo.com>
2019-09-28 16:07:27 +1000

    Setup Tests for the rest

--- restful-web-services/src/test/java/com/sept/rest/webservices/restfulwebservices/WallBeanControllerTest.java
+++ restful-web-services/src/test/java/com/sept/rest/webservices/restfulwebservices/WallBeanControllerTest.java
@@ -1,4 +1,22 @@
 package com.sept.rest.webservices.restfulwebservices;
 
+import com.sept.rest.webservices.restfulwebservices.UserBean.UserBeanRepository;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class WallBeanControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserBeanRepository mockRepository;
 }
