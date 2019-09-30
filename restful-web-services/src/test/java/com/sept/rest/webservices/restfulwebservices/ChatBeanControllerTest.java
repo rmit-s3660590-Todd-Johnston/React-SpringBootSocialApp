4c5a7ba0b0a19e29c2018164e36723e53393c4ca
Shahrzad Rafezi <shahrzad_rafezi@yahoo.com>
2019-09-30 12:51:30 +1000

    Fixed up GroupBean Test but still there's one error

--- restful-web-services/src/test/java/com/sept/rest/webservices/restfulwebservices/ChatBeanControllerTest.java
+++ restful-web-services/src/test/java/com/sept/rest/webservices/restfulwebservices/ChatBeanControllerTest.java
@@ -15,8 +15,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
--- restful-web-services/src/test/java/com/sept/rest/webservices/restfulwebservices/GroupBeanControllerTest.java
+++ restful-web-services/src/test/java/com/sept/rest/webservices/restfulwebservices/GroupBeanControllerTest.java
@@ -1,12 +1,11 @@
 package com.sept.rest.webservices.restfulwebservices;
 
-import com.sept.rest.webservices.restfulwebservices.ChatBean.ChatBean;
 import com.sept.rest.webservices.restfulwebservices.GroupBean.GroupBean;
 import com.sept.rest.webservices.restfulwebservices.GroupBean.GroupBeanRepository;
 import com.sept.rest.webservices.restfulwebservices.UserBean.UserBean;
 import com.sept.rest.webservices.restfulwebservices.UserBean.UserBeanRepository;
 import com.sept.rest.webservices.restfulwebservices.WallBean.WallBean;
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,28 +29,28 @@ public class GroupBeanControllerTest {
     private GroupBeanRepository groupBeanRepository;
     @MockBean
     private UserBeanRepository mockRepository;
-    @BeforeClass
-    void sampleData()
-    {
-        mockRepository.save(new UserBean((long) 1, "sept", "Test1", "Jeffery", "password", false, "https://pbs.twimg.com/media/Dfbui6uWAAAmSb-.jpg"));
-        mockRepository.save(new UserBean((long) 2, "PeppaPig", "Peppa", "Pig", "password", false, "https://mediad.publicbroadcasting.net/p/shared/npr/styles/x_large/nprshared/201908/746995873.jpg"));
-        mockRepository.save(new UserBean((long) 3, "testMentor", "TestMentor1", "Mentor", "password", true, "fakeurl"));
-        mockRepository.save(new UserBean((long) 4, "testMentor2", "TestMentor2", "Mentor", "password", true,"fakeurl1"));
-        WallBean wall1 = new WallBean((long)1, "Post1");
-        GroupBean group1 = new GroupBean((long)1, "Group1", (long) 1);
-        groupBeanRepository.save(group1);
 
+    @Before
+   public void sampleData() {
+        mockRepository.save(new UserBean( 1L, "sept", "Test1", "Jeffery", "password", false, "https://pbs.twimg.com/media/Dfbui6uWAAAmSb-.jpg"));
+        mockRepository.save(new UserBean( 2L, "PeppaPig", "Peppa", "Pig", "password", false, "https://mediad.publicbroadcasting.net/p/shared/npr/styles/x_large/nprshared/201908/746995873.jpg"));
+        mockRepository.save(new UserBean(3L, "testMentor", "TestMentor1", "Mentor", "password", true, "fakeurl"));
+        mockRepository.save(new UserBean( 4L, "testMentor2", "TestMentor2", "Mentor", "password", true,"fakeurl1"));
+        WallBean wall1 = new WallBean(1L, "Post1");
+        GroupBean group1 = new GroupBean(1L, "Group1",  1L);
+        groupBeanRepository.save(group1);
     }
 
     @Test
-    void checkNotNull()
-    {
+    public void checkNotNull() {
         assertThat(mockRepository).isNotNull();
         assertThat(groupBeanRepository).isNotNull();
-        assertThat(groupBeanRepository.findById((long)1).get()).isNotNull();
+        assertThat(groupBeanRepository.findById(1L)).isNotNull();
     }
+
     @Test
     public void findById(){
-        assertThat(groupBeanRepository.findById((long)1).get().getId()).isEqualTo((long)1);
+        assertThat(groupBeanRepository.findById(1L).get().getId()).isEqualTo(1L);
+        //give an error saying "No value present"
     }
 }
