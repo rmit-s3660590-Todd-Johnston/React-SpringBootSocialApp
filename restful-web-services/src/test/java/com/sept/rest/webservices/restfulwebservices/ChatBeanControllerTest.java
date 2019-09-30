65c5113c0f9e64c388f1c448b78f6f1c0deacf57
Shahrzad Rafezi <shahrzad_rafezi@yahoo.com>
2019-09-30 12:24:38 +1000

    Fixed the Long id in GroupBean Test

--- restful-web-services/src/test/java/com/sept/rest/webservices/restfulwebservices/ChatBeanControllerTest.java
+++ restful-web-services/src/test/java/com/sept/rest/webservices/restfulwebservices/ChatBeanControllerTest.java
@@ -33,24 +33,21 @@ public class ChatBeanControllerTest {
     private ChatBeanRepository chatRepository;
     @MockBean
 
-
     UserBean mockUser = new UserBean(1L, "Sherry1377", "Shahrzad", "Rafezi", "Password1234!", false,"gfg");
 
     @BeforeClass
-    void sampleData()
-    {
-        mockRepository.save(new UserBean((long) 1, "sept", "Test1", "Jeffery", "password", false, "https://pbs.twimg.com/media/Dfbui6uWAAAmSb-.jpg"));
-        mockRepository.save(new UserBean((long) 2, "PeppaPig", "Peppa", "Pig", "password", false, "https://mediad.publicbroadcasting.net/p/shared/npr/styles/x_large/nprshared/201908/746995873.jpg"));
-        mockRepository.save(new UserBean((long) 3, "testMentor", "TestMentor1", "Mentor", "password", true, "fakeurl"));
-        mockRepository.save(new UserBean((long) 4, "testMentor2", "TestMentor2", "Mentor", "password", true,"fakeurl1"));
-        chatRepository.save(new ChatBean((long) 1,null));
-        chatRepository.save(new ChatBean((long) 2,null));
-        chatRepository.findById((long)1).get().addUserBean((long)1);
+    void sampleData() {
+        mockRepository.save(new UserBean( 1L, "sept", "Test1", "Jeffery", "password", false, "https://pbs.twimg.com/media/Dfbui6uWAAAmSb-.jpg"));
+        mockRepository.save(new UserBean( 2L, "PeppaPig", "Peppa", "Pig", "password", false, "https://mediad.publicbroadcasting.net/p/shared/npr/styles/x_large/nprshared/201908/746995873.jpg"));
+        mockRepository.save(new UserBean( 3L, "testMentor", "TestMentor1", "Mentor", "password", true, "fakeurl"));
+        mockRepository.save(new UserBean( 4L, "testMentor2", "TestMentor2", "Mentor", "password", true,"fakeurl1"));
+        chatRepository.save(new ChatBean( 1L,null));
+        chatRepository.save(new ChatBean( 2L,null));
+        chatRepository.findById(1L).get().addUserBean(1L);
     }
 
     @Test
-    void checkNotNull()
-    {
+    void checkNotNull() {
         assertThat(mockRepository).isNotNull();
         assertThat(chatRepository).isNotNull();
         assertThat(chatRepository.findById((long)1).get()).isNotNull();
@@ -58,8 +55,8 @@ public class ChatBeanControllerTest {
 
     @Test
     public void findById(){
-        ArrayList<Long> userids = new ArrayList<>();
-        userids.add((long)1);
-        assertThat(chatRepository.findById((long)1).get().getUserIds()).isEqualTo(userids);
+        ArrayList<Long> userIds = new ArrayList<>();
+        userIds.add((long)1);
+        assertThat(chatRepository.findById((long)1).get().getUserIds()).isEqualTo(userIds);
     }
 }
