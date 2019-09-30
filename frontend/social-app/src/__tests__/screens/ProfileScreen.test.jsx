b9b89988480b805c6caff7a882b3cc1fea97dadb
Jack <jackryan1998@hotmail.com>
2019-09-30 13:20:59 +1000

    Revert "error merge"
    This reverts commit 27322e9c382f84658754c77e83812e95928819e9.

--- frontend/social-app/src/__tests__/screens/ProfileScreen.test.jsx
+++ frontend/social-app/src/__tests__/screens/ProfileScreen.test.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDom, {unmountComponentAtNode} from 'react-dom';
 import { act } from "react-dom/test-utils";
 import ProfileScreen from "../../screens/ProfileScreen";
-import ProfileScreen from "../screens/ProfileScreen";
-import {Modal} from "antd";
 
 let container = null;
 beforeEach(() => {
@@ -23,12 +21,5 @@ it("Testing inserting Div into profile screen", () => {
     act(() => {
         ReactDom.render(<ProfileScreen/>, container);
     });
-    expect(container.textContent).toBe( "ProfileMarkMessage MeAdd Friend?About MeUserNameMarkTelephone9876543234CityCaliAddress Planet EarthMy SkillsBusiness ManDecision MakerDeal CloserOffer MakerMachine Learning");
-});
-
-it('should ', () => {
-    act(() => {
-        ReactDom.render(<Modal></Modal>)
-    });
-
+    expect(container.textContent).toBe( "ProfileName ErrorGo To WallMessage MeAdd to study groupAdd Friend?About MeNameMarkTelephone9876543234CityCaliAddress Planet EarthMy SkillsBusiness ManDecision MakerDeal CloserOffer MakerMachine Learning");
 });
